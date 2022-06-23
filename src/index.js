05a8c263bb728aff997a5566c8b1a155d3c7a165
Rafael Caseiro <rafae_lbp@hotmail.com>
2022-06-23 14:43:46 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "https://kit.fontawesome.com/2fcdfab0bd.js";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 
